8ff5bc658d7ec53422c466e7510f268e91f95530
alp <alp@ankageo.com>
2016-10-01 19:01:57 +0300

    ilk

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://www.posbee.co.uk/demo/mobileapp/api",
+	'DialogDefaultTitle' : "Sample Restaurant",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'facebookAppId' : "1582616208711614",
+	'APIHasKey' : "83b4f817be44d126421445cb61f1c6128d3a"
+};
